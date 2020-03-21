ad9c1cd980bed37a743174ae06d9b2fd8bcd97f6
eugeniagalarza <eugeniagalarza@outlook.com>
2020-03-21 15:41:49 -0300

    Diseño de login

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -2,6 +2,23 @@ import React, {useState} from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import Layout from "../core/Layout";
 import { signin, authenticate, isAuthenticated } from '../auth';
+import {
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBIcon,
+    MDBCardHeader,
+    MDBBtn,
+    MDBInput
+  } from "mdbreact";
+  import Background from '../resources/empresa.jpg';
+
+
+var sectionStyle = {
+  backgroundImage: `url(${Background})`
+};
 //import { set } from 'mongoose';
 
 
@@ -42,21 +59,79 @@ const Signin = () => {
     };
 
     const signUpForm = () => (
-        <form>
-            <div className="form-group">
-                <label className="text-muted">Email</label>
-                <input onChange={handleChange('email')} type="email" className="form-control" placeholder={email}/>
-            </div>
-            
-            <div className="form-group">
-                <label className="text-muted">Password</label>
-                <input onChange={handleChange('password')} type="password" className="form-control" placeholder={password}/>
-            </div>
 
-            <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
+<div className="m-auto" style={sectionStyle}>
+<MDBContainer >
+      <MDBRow>
+        <MDBCol md="6" className="m-auto ">
+          <MDBCard className="mb-5 mt-5">
+            <MDBCardBody>
+              <MDBCardHeader className="form-header deep-blue-gradient rounded">
+                <h3 className="my-3">
+                  <MDBIcon icon="lock" /> Ingreso
+                </h3>
+              </MDBCardHeader>
+              <form>
+                <div className="grey-text">
+                  <MDBInput
+                    label="Correo"
+                    icon="envelope"
+                    group
+                    type="email"
+                    validate
+                    error="wrong"
+                    success="right"
+                    onChange={handleChange('email')}
+                  />
+                  <MDBInput
+                    label="Contraseña"
+                    icon="lock"
+                    group
+                    type="password"
+                    validate
+                    onChange={handleChange('password')}
+                  />
+                </div>
+
+              <div className="text-center mt-4">
+                <MDBBtn
+                  color="light-blue"
+                  className="mb-3"
+                  type="submit"
+                  onClick={clickSubmit}
+                >
+                  Ingresar
+                </MDBBtn>
+              </div>
+              </form>
+              <hr></hr>
+              <div className="font-weight-light text-center">
+                  <p>No contas con un usuario?  <Link className=""  to="/signup">Registrarte</Link></p>  
+                  <p>No recuerdas la contraseña?  <Link className=""  to="/signup">Recuperar Contraseña</Link> </p>
+                </div>
+        
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+    </div>
+
+        // <form>
+        //     <div className="form-group">
+        //         <label className="text-muted">Email</label>
+        //         <input onChange={handleChange('email')} type="email" className="form-control" placeholder={email}/>
+        //     </div>
+            
+        //     <div className="form-group">
+        //         <label className="text-muted">Password</label>
+        //         <input onChange={handleChange('password')} type="password" className="form-control" placeholder={password}/>
+        //     </div>
+
+        //     <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
             
 
-        </form>
+        // </form>
     );
 
     const showError = () => (
@@ -89,7 +164,7 @@ const Signin = () => {
 
        
     return(
-        <Layout title="SignIn" description="SignIn to Ecommerce" className="container col-md-8 offset-md-2">
+        <Layout title="SignIn" description="SignIn to Ecommerce" className="">
             { showLoading() }
             { showError() }
             { signUpForm() }
