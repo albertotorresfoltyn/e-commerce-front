655e49024cc856390d397e539c7125eb51be2392
eugeniagalarza <eugeniagalarza@outlook.com>
2020-03-28 13:07:16 -0300

    busqueda

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -66,6 +66,15 @@ const Shop = props => {
     );
   };
 
+  const reloadCall = (param) => {
+    
+    var indexDelete = filter.findIndex(item => item === param);
+    filter.splice(indexDelete, 1);  
+    var filtrosUpdated = filter.join(" ");
+    window.location.href =  "http://localhost:3000/shop/" + filtrosUpdated;
+
+  };
+
   //populates the categories on the sidebar
   useEffect(() => {
     init();
@@ -117,6 +126,7 @@ const Shop = props => {
                       key={tag}
                       color="primary"
                       className="rounded-0"
+                      onClick={ ()=>{reloadCall(tag)}}
                     >
                       {tag}
                     </MDBChip>
